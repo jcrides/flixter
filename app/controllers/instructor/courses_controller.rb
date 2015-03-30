8d5fb5a48c9e699e2bbdbf31096604b0f1a5e342
Jeremy Carey <jeremycar@gmail.com>
2015-03-29 22:42:05 -0700

    add course model, instructor/courses controller and views (lsn 12)

---
+++ app/controllers/instructor/courses_controller.rb
@@ -0,0 +1,26 @@
+class Instructor::CoursesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = current_user.courses.create(course_params)
+    redirect_to instructor_course_path(@course)
+  end
+
+  def show
+    @course = Course.where(:id => params[:id]).first
+    if @course.blank?
+      render :text => 'Course not found.', :status => :not_found
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
+
+end
