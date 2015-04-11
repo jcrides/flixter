b57260558804eef8d7a3f9c5b801b164e2643883
Jeremy Carey <jeremycar@gmail.com>
2015-04-10 23:48:56 -0700

    configure carrierwave initializer to only configure fog for production

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,10 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      provider:                         'Google',
-      google_storage_access_key_id:     ENV['ACCESS_KEY_ID'],
-      google_storage_secret_access_key: ENV['ACCESS_KEY_SECRET']
-  }
-  config.fog_directory = 'nom-images'
+  if Rails.env.production?
+    config.fog_credentials = {
+        provider:                         'Google',
+        google_storage_access_key_id:     ENV['ACCESS_KEY_ID'],
+        google_storage_secret_access_key: ENV['ACCESS_KEY_SECRET']
+    }
+    config.fog_directory = 'nom-images'
+  end
 end
